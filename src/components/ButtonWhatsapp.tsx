9b0089f2847dc7d7b3cbd4b46643367906348701
unknown <allandiego252@gmail.com>
2023-04-06 17:24:36 -0300

    include pulsate

--- src/components/ButtonWhatsapp.tsx
+++ src/components/ButtonWhatsapp.tsx
@@ -7,6 +7,7 @@ export default function ButtonWhatsapp() : JSX.Element {
 
   return (
     <Link className="fixed bg-white rounded-full right-16 bottom-10 z-30" href={"https://wa.me/" + number + "?text=" + text} target="_blank" aria-label="Gostaria de solicitar um orçamento de construção.">
+       <span className="animate-ping delay-150 absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
       <IoLogoWhatsapp className="text-green-500" size={56}/>
     </Link>
   );
