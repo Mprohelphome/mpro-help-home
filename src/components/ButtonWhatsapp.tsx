e3446b623081ca9955cabe6bb0dd80c710a1ecc4
unknown <allandiego252@gmail.com>
2023-04-13 22:22:11 -0300

    include marketing id

--- src/components/ButtonWhatsapp.tsx
+++ src/components/ButtonWhatsapp.tsx
@@ -6,7 +6,7 @@ export default function ButtonWhatsapp() : JSX.Element {
   const text = "Olá, gostaria de solicitar um atendimento."; 
 
   return (
-    <Link className="fixed bg-white rounded-full right-16 bottom-10 z-30" href={"https://wa.me/" + number + "?text=" + text} target="_blank" aria-label="Gostaria de solicitar um orçamento de construção.">
+    <Link className="fixed bg-white rounded-full right-16 bottom-10 z-30" id="whatsapp-fluttuante" href={"https://wa.me/" + number + "?text=" + text} target="_blank" aria-label="Gostaria de solicitar um orçamento de construção.">
        <span className="animate-ping delay-150 absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
       <IoLogoWhatsapp className="text-green-500" size={56}/>
     </Link>
